52a5eb7c4477b3bd9105e443f5495ef47cee9752
vemuladevendran <devendranvemula@gmail.com>
2025-03-14 11:03:16 -0700

    profile page design and general icons added

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -11,6 +11,56 @@ import { IonHeader, IonToolbar, IonTitle } from "@ionic/angular/standalone";
 })
 export class ProfilePage implements OnInit {
 
+
+  regularlyUsedIcons: any = [
+    {
+      name: 'Category',
+      path: 'category.svg'
+    },
+    {
+      name: 'Budget',
+      path: 'budget.svg'
+    },
+    {
+      name: 'Calendar',
+      path: 'calendar.svg'
+    },
+    {
+      name: 'Ledger',
+      path: 'ledger.svg'
+    },
+    {
+      name: 'Savings',
+      path: 'savings.svg'
+    },
+    {
+      name: 'Tag',
+      path: 'tag.svg'
+    },
+  ]
+
+  otherIcons: any = [
+    {
+      name: 'Themes',
+      path: 'themes.svg'
+    },
+    {
+      name: 'Sync Data',
+      path: 'sync.svg'
+    },
+    {
+      name: 'Share App',
+      path: 'share.svg'
+    },
+    {
+      name: 'Reminders',
+      path: 'reminder.svg'
+    },
+    {
+      name: 'Privacy Policy',
+      path: 'policy.svg'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
