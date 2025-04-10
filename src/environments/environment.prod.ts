ef8c554a6d7784446028557de64667e51d587cfd
vemuladevendran <devendranvemula@gmail.com>
2025-04-09 23:24:54 -0700

    new backend host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
    // API_HOST: 'http://localhost:3000',
-   API_HOST: 'https://aplan-my-money-api.onrender.com',
+  //  API_HOST: 'https://aplan-my-money-api.onrender.com',
+   API_HOST: 'https://ahtlauvd.a2hosted.com',
   API_BASE: '/api/v1',
   splitWiseClientId: 'O5rkDFbdsCrzCuvElISyBpS803i4ZYG5m36raFUk',
   splitWiseClientSecret: 'w0YrM4Tx94AuOGSM2v7ePPazNFCVmzI9xnhl1DLm',
